13faa43529c0b7b83d3811bec8557952305290f0
nazi sharifi <banonazisharifi@gmail.com>
2026-04-24 09:35:46 +0430

    add migrations files

---
+++ database/factories/FilePermissionsFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\file_permissions;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<file_permissions>
+ */
+class FilePermissionsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
