f88daa3541a3dc9cf9dabc252ab89e8baa4c71b3
nazi sharifi <banonazisharifi@gmail.com>
2026-04-24 09:56:23 +0430

    add relationship

--- app/Models/file_permissions.php
+++ app/Models/file_permissions.php
@@ -9,4 +9,17 @@ class file_permissions extends Model
 {
     /** @use HasFactory<\Database\Factories\FilePermissionsFactory> */
     use HasFactory;
+    protected $fillable = [
+        "file_id",
+        "user_id",
+        "can_read",
+        "can_delete",
+        "can_update",
+        "can_write",
+        "can_print",
+        "can_copy",
+    ];
+    public function user(){
+        return $this->belongsTo(User::class , "user_id");
+    }
 }
